60e3deeed4603856bb3dd6083c51d7266dd432fc
LGM <farhadi94@gmail.com>
2013-02-26 15:49:30 +0330

    Score Bar

---
+++ src/javachallenge/graphics/components/ScoreBar.java
@@ -0,0 +1,34 @@
+package javachallenge.graphics.components;
+
+import java.awt.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LGM
+ * Date: 2/26/13
+ * Time: 3:17 PM
+ */
+public class ScoreBar extends Panel
+{
+	protected ProgressBar bar;
+	protected Label status;
+	protected String name;
+	public ScoreBar(int x, int y, int w, int h, Color fill,Color empty,String name)
+	{
+		super(x, y, w, h);
+		bar=new ProgressBar(0,30,w,h-30,fill,empty,0);
+		add(bar);
+		this.name=name;
+		(status=new Label()).setBounds(0,0,w,30);
+		setScore(0);
+		add(status);
+	}
+	public void updateBar(double ratio)
+	{
+		bar.updateProgressBar(ratio);
+	}
+	public void setScore(int score)
+	{
+		status.setText(name+": "+score);
+	}
+}
