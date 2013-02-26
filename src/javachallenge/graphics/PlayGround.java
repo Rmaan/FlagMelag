a583d43c0c0ac66cf8aa90619a8b295d909d25d5
LGM <farhadi94@gmail.com>
2013-02-26 22:00:40 +0330

    ???

--- src/javachallenge/graphics/PlayGround.java
+++ src/javachallenge/graphics/PlayGround.java
@@ -68,7 +68,7 @@ public class PlayGround extends Screen {
 			logMonitor.setLocation(10, sidebar.getHeight() / 2);
 			logMonitor.setSize(sidebar.getWidth() - 20, sidebar.getHeight() / 2 - 10);
 			logMonitor.getScroll().setSize(sidebar.getWidth() - 20, sidebar.getHeight() / 2 - 10);
-			//status.updatePosition();
+			logMonitor.getScroll().revalidate();
 		}
 	}
 	
