d5cf85978c2aae229a32afd7bba04d8cd679c8fe
Mina Tahmasbi <mina.tahmasbi@gmail.com>
2013-02-28 05:30:12 +0330

    sample player

--- src/javachallenge/client/myplayer/MyPlayer.java
+++ src/javachallenge/client/myplayer/MyPlayer.java
@@ -1,69 +1,28 @@
 package javachallenge.client.myplayer;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 import javachallenge.client.Agent;
 import javachallenge.client.Player;
-import javachallenge.common.BlockType;
 import javachallenge.common.CycleAction;
 import javachallenge.common.Direction;
-import javachallenge.common.Point;
-import javachallenge.common.VisionPoint;
 
 public class MyPlayer extends Player {
 	int cycle = 0;
 	Random rnd = new Random();
 	
-	void moveTo(Agent agent, Point p) {
-		int ay = agent.getLocation().y;
-		int ax = agent.getLocation().x;
-		
-		if (Math.abs(p.y - ay) >= 1) {
-			agent.doMove(ay > p.y ? Direction.NORTH : Direction.SOUTH);
-			return;
-		}
-		
-		if (p.x > ax) {
-			agent.doMove(ax % 2 == 0 ? Direction.SOUTHEAST : Direction.NORTHEAST);
-		}
-		
-		if (p.x < ax) {
-			agent.doMove(ax % 2 == 0 ? Direction.SOUTHWEST : Direction.NORTHWEST);
-		}
-	}
 	
 	public void step() {
-		Point goal = new Point(12, 12);
-//		Direction[] attackDir1 = {Direction.NORTHWEST, Direction.SOUTHEAST, Direction.NORTHWEST, Direction.SOUTHWEST, Direction.NONE, Direction.NONE};
-//		Direction[] attackDir2 = {Direction.NONE, Direction.NORTHEAST, Direction.SOUTHEAST, Direction.NONE, Direction.NONE, Direction.NONE};
-		Direction[] attackDir1 = {Direction.NORTHWEST, Direction.SOUTH};
-		ArrayList<Integer> ids = getAgentIds();
-		System.out.println(getWorld().getFlagOwners());
-		System.out.println(getWorld().getPowerups());
-		for (int i = 0 ; i < ids.size() ; i++) {
+		for (Integer id : getAgentIds()) {
 			
-			Agent agent = this.getAgentById(ids.get(i));
+			Agent agent = this.getAgentById(id);
 			
 			if (getCycleAction() == CycleAction.MOVE_CYCLE)
 				agent.doMove(Direction.values()[rnd.nextInt(Direction.values().length)]);
 			else
 				agent.doAttack(Direction.values()[rnd.nextInt(Direction.values().length)]);
 			
-			//moveTo(agent, goal);
-//			if(getCycleAction() == CycleAction.MOVE_CYCLE){
-//				agent.doMove(Direction.values()[rnd.nextInt(Direction.values().length)]);
-//			}
-//			else{
-//				if(cycle > 50){
-//					if(getTeamId() == 0){
-//						throw new RuntimeException();
-//					}
-//				}
-//			}
-			
 		}
-		cycle++;
 	}
 
 	@Override
